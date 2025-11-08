294f32e2f3f86df25f4768910d6f138b2170f825
Saitama <124196161+saitamapunches123@users.noreply.github.com>
2025-11-08 09:49:50 +0000

    Added ternary representation

--- Bit Operations/ternary-representations.cpp
+++ Bit Operations/ternary-representations.cpp
@@ -6,8 +6,22 @@ For example:
 15 in ternary = 120 =1*3^2+2*3^1+0*3^0
 
 
-*/
+Why is ternart usefull?
 
+as we increase the base the number of digits decreases required to represent a number.
+to represent up to n we need logb(n)+1 digits in base b.
+
+and the size of set we need to represent all values in 0ton = (logb(n)+1)*(b-1)  // max power*(b-1) 
+as for each power we need 1*b^p , 2*b^p,3*b^p.... (b-1)*b^p
+
+this size of set can be mimumum at 3 as for example here: 
+https://www.codechef.com/problems/MAKEALL
+
+*/
+int digitsInBase(long long n, int base) {
+    if (n == 0) return 1;
+    return floor(log(n) / log(base)) + 1;
+}
 
 
 // Function to convert an integer to its ternary representation (vector of ints)
@@ -15,13 +29,13 @@ vector<int> toTernary(int n) {
     if (n == 0) {
         return {0};
     }
-
     vector<int> ternary;
     while (n > 0) {
         ternary.push_back(n % 3);
         n /= 3;
     }
-
     reverse(ternary.begin(), ternary.end());
     return ternary;
 }
+
+
